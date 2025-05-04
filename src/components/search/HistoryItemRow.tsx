9ad0ccb232340877c41cef1474825aebe6ea0855
GKGB1212 <binhkhongg@gmail.com>
2025-05-04 13:55:08 +0700

    fix > update button color

--- src/components/search/HistoryItemRow.tsx
+++ src/components/search/HistoryItemRow.tsx
@@ -15,13 +15,13 @@ const HistoryItemRow: React.FC<HistoryItemRowProps> = ({ item, onClick, onDelete
       <div className="flex gap-1 sm:gap-2">
         <button
           onClick={() => onClick(item)}
-          className="aspect-square p-1 sm:p-2 bg-gray-100 text-black rounded-md hover:bg-gray-200"
+          className="aspect-square p-1 sm:p-2 transition-all duration-100 text-black rounded-md hover:bg-gray-200"
         >
           <MagnifyingGlass weight="bold" size={16} />
         </button>
         <button
           onClick={() => onDelete(item.name)}
-          className="aspect-square p-1 sm:p-2 bg-gray-100 text-black rounded-md hover:bg-gray-200"
+          className="aspect-square p-1 sm:p-2 transition-all duration-100  text-black rounded-md hover:bg-gray-200"
         >
           <TrashSimple weight="bold" size={16} />
         </button>
